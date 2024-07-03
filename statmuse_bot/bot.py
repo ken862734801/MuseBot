a67546139ca7e7ec73401f552c3a9d6b7f0ad8d5
ken862734801 <kenshawmichaud@gmail.com>
2024-07-02 23:57:01 -0400

    add command parser

--- statmuse_bot/bot.py
+++ statmuse_bot/bot.py
@@ -1,10 +1,39 @@
 from twitchio.ext import commands
+from nba.stat import Stat
+
+stat = Stat()
 
 class Bot(commands.Bot):
     
     def __init__(self, token):
         super().__init__(token=token, prefix="!", initial_channels=[''])
 
+    def parse_command(self, message):
+        words = message.split()
+        if len(words) > 1:
+            name = ' '.join(words[1:])
+            return name
+        else:
+            return None
+
     async def event_ready(self):
         print(f'Logged in as: {self.nick}')
+        print(f'User id is: {self.user_id}')
+
+    @commands.command(name='career')
+    async def career(self, ctx: commands.Context):
+        name = self.parse_command(ctx.message.content)
+        if name:
+            await ctx.send(f'{name}')
+        else:
+            await ctx.send('Incorrect usage!')
+    
+    @commands.command(name='record')
+    async def record(self, ctx: commands.Context):
+        team = self.parse_command(ctx.message.content)
+        if team:
+            await ctx.send(f'{team}')
+        else:
+            await ctx.send(f'Incorrect usage!')
+
 
